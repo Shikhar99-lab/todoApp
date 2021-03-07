58e2e700bd70ae23254e608b0baeafb78042d42b
Shikhar99 <me.shikhar99@gmail.com>
2021-03-07 13:05:48 +0530

    inital commit

---
+++ build/precache-manifest.ea785f112efcefa4189c3f3c84af1057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abacb9b2ce4547bf7a203eeef7e1771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef0d60c4ac87c275f75",
+    "url": "/static/css/main.1f25eaaf.chunk.css"
+  },
+  {
+    "revision": "d09401dd82f4d495427a",
+    "url": "/static/js/2.ad5aac3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad5aac3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef0d60c4ac87c275f75",
+    "url": "/static/js/main.23f7e27d.chunk.js"
+  },
+  {
+    "revision": "d19e813aadc2807b3ea9",
+    "url": "/static/js/runtime-main.3a9c02fd.js"
+  },
+  {
+    "revision": "07be24fe1e079f096d27e0875d1027f5",
+    "url": "/static/media/circle-cropped.07be24fe.png"
+  }
+]);
